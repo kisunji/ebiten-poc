18ac93ccc6f9262947a44b624d2e05155c44a1b4
Chris Kim <kisunji92@gmail.com>
2020-12-16 21:59:23 -0500

    Add rest animation

--- main.go
+++ main.go
@@ -94,13 +94,49 @@ func (r *Runner) Draw(screen *ebiten.Image, clock int) {
 		r.py-frameHeight/2,
 	)
 
-	i := clock % frameNum
-	sx, sy := frameOX+i*frameWidth, frameOY
-	screen.DrawImage(
-		runnerImage.SubImage(
-			image.Rect(sx, sy, sx+frameWidth, sy+frameHeight)).(*ebiten.Image),
-		op,
+	var sprite *ebiten.Image
+	if r.vx != 0 || r.vy != 0 {
+		sprite = runnerWalkingFrame(clock)
+	} else {
+		sprite = runnerWaitingFrame(clock)
+	}
+
+	screen.DrawImage(sprite, op)
+}
+
+func runnerWalkingFrame(clock int) *ebiten.Image {
+	const (
+		frameNum    = 8
+		frameWidth  = 32
+		frameHeight = 32
+		frameOX     = 0
+		frameOY     = 32
 	)
+
+	i := clock % frameNum
+	sx := frameOX + i*frameWidth
+	sy := frameOY
+
+	return runnerImage.SubImage(
+		image.Rect(sx, sy, sx+frameWidth, sy+frameHeight),
+	).(*ebiten.Image)
+}
+
+func runnerWaitingFrame(clock int) *ebiten.Image {
+	const (
+		frameNum    = 5
+		frameWidth  = 32
+		frameHeight = 32
+		frameOX     = 0
+		frameOY     = 0
+	)
+	i := clock / 2 % frameNum
+	sx := frameOX + i*frameWidth
+	sy := frameOY
+
+	return runnerImage.SubImage(
+		image.Rect(sx, sy, sx+frameWidth, sy+frameHeight),
+	).(*ebiten.Image)
 }
 
 type Game struct {
